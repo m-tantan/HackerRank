fc22e8a5bb16c8d11ca378aadd5ea043b1db60bc
Yonatan Manor <Yonatan.Manor@mail.huji.ac.il>
2018-06-07 11:48:59 +0300

    Initial commit

---
+++ TimeConversionAM-PM.cpp
@@ -0,0 +1,98 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using std::cout ;
+using std::endl ;
+using std::string;
+using std::cin;
+
+using std::to_string;
+using std::ofstream;
+
+
+string timeConversion(string s) {
+    string h = "", m = "", sc = "";
+    string frontZero = "0";
+
+    int nColons = 0;
+    bool midnight = false;
+    bool pastNoon = false;
+    for (char* c = &s[0]; *c != '\0'; ++c)
+    {
+
+        if (*c == 'P')
+        {
+            pastNoon = true;
+            break;
+        }
+        if(*c == 'A') {
+            break;
+        }
+        if (*c == ':')
+        {
+            ++nColons;
+            continue;
+        }
+        if (nColons == 0)
+        {
+            h += *c;
+        }
+        if (nColons == 1)
+        {
+            m += *c;
+        }
+        if (nColons == 2)
+        {
+            sc += *c;
+        }
+
+    }
+
+    int hours = stoi(h);
+    if (pastNoon)
+    {
+        if (hours != 12)
+            hours += 12;
+
+        if (hours == 36)
+        {
+            hours = 0;
+        }
+
+    }
+    else {
+        if (hours == 12) {
+            hours = 0;
+        }
+    }
+    if (hours == 0)
+    {
+        h = to_string(0) + to_string(0);
+    }
+    else {
+        h = to_string(hours);
+    }
+    if (h.length() == 1)
+        h = to_string(0) + h;
+
+
+    return h + ":" + m + ":" + sc;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = timeConversion(s);
+
+    cout<<  result << "\n";
+
+    fout.close();
+
+    return 0;
+}
